c6a1b43373d0bbc1eaded3f5821aab5edf1cd30b
kannanind20-cloud <kannanind20@gmail.com>
2026-03-18 19:33:44 +0530

    chrome

--- Sample1/Feature_File/environment.py
+++ Sample1/Feature_File/environment.py
@@ -1,10 +1,16 @@
+from email.header import Header
+
 from selenium import webdriver
 from Sample1.Feature_File.POM.Page_Keyword import Pagekeywords
-
+from selenium.webdriver.chrome.options import Options
 
 def before_all(context):
-    context.driver = webdriver.Chrome()
+    options = Options()
+    # options.add_argument("--headless")  # ❌ keep this commented if you want UI
+
+    context.driver = webdriver.Chrome(options=options)
     context.driver.maximize_window()
+
     context.PKEY = Pagekeywords(context)
 
 
