34db18153c8da13f3a324895be803ee40c5aebed
kannanind20-cloud <kannanind20@gmail.com>
2026-03-18 19:55:40 +0530

    Report

--- Sample1/Runner.py
+++ Sample1/Runner.py
@@ -1,8 +1,11 @@
 from behave.__main__ import main
 
 if __name__ == "__main__":
-    main("Sample1/Feature_File")
-
+    main([
+        "Sample1/Feature_File",
+        "--format", "html",
+        "--outfile", "reports/report.html"
+    ])
 # from behave.__main__ import main as Behave
 # if __name__ == '__main__':
 #     Log= r"C:\Users\kkannan\PyCharmMiscProject\Sample1\Feature_File"
